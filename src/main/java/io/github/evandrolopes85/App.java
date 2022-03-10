c0e26f30d289b149d6ff8597c72af6448f7e7ddd
evandrolopes85 <evandrocwd@gmail.com>
2022-03-10 02:24:53 -0300

    Adicionando loop

--- src/main/java/io/github/evandrolopes85/App.java
+++ src/main/java/io/github/evandrolopes85/App.java
@@ -5,38 +5,47 @@ import java.util.Scanner;
 public class App {
 	public static void main(String[] args) {
 		Scanner entrada = new Scanner(System.in);
+		int opcao = 0;
+		
+		do {
 
-		System.out.println("Selecione o exercicio");
-		System.out.println("1 - Escada");
-		System.out.println("2 - Validacao");
-		System.out.println("3 - Anagrama");
-		int op = entrada.nextInt();
+			System.out.println("Selecione o exercicio");
+			System.out.printf("%2d - Exercicio 1 - Escada\n", 1);
+			System.out.printf("%2d - Exercicio 2 - Validacao\n", 2);
+			System.out.printf("%2d - Exercicio 3 - Anagrama\n", 3);
+			System.out.printf("%2d - Sair\n", -1);
 
-		switch (op) {
-		case 1: {
-			System.out.println("Digite o tamanho da escada");
-			entrada.nextLine();
-			int tamanho = entrada.nextInt();
-			System.out.println(Escada.exibirEscada(tamanho));
-			break;
-		}
-		case 2: {
-			System.out.println("Digite a senha");
-			entrada.nextLine();
-			String str = entrada.nextLine();
-			if (!SenhaForte.validandoTamanho(str)) {
-				System.out.printf("%d\n", 6 - str.length());
+			opcao = entrada.nextInt();
+			
+			if(opcao == -1) break;
+
+			switch (opcao) {
+			case 1: {
+				System.out.println("Digite o tamanho da escada");
+				entrada.nextLine();
+				int tamanho = entrada.nextInt();
+				System.out.println(Escada.exibirEscada(tamanho));
+				break;
 			}
-			break;
-		}
-		case 3: {
-			System.out.println("Digite a palavra");
-			entrada.nextLine();
-			String str = entrada.nextLine();
-			System.out.println(Anagrama.quantidadeDeAnagramas(str));
-			break;
-		}
-		}
-
+			case 2: {
+				System.out.println("Digite a senha");
+				entrada.nextLine();
+				String str = entrada.nextLine();
+				if (!SenhaForte.validandoTamanho(str)) {
+					System.out.printf("%d\n", 6 - str.length());
+				}
+				break;
+			}
+			case 3: {
+				System.out.println("Digite a palavra");
+				entrada.nextLine();
+				String str = entrada.nextLine();
+				System.out.println(Anagrama.quantidadeDeAnagramas(str));
+				break;
+			}
+			}
+			
+			System.out.println();
+		} while (true);
 	}
 }
